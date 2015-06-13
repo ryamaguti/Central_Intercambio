4d1181d5d5a276c595da43d86e3281c38739e4ac
ryamaguti <rnd_mc@hotmail.com>
2015-06-13 14:47:07 -0300

    teste

--- Cliente_CI.HTMLClient/GeneratedArtifacts/viewModel.intellisense.js
+++ Cliente_CI.HTMLClient/GeneratedArtifacts/viewModel.intellisense.js
@@ -58,6 +58,47 @@
         ScreenContent_render: [$element, function () { return new lightSwitchApplication.Home().findContentItem("ScreenContent"); }]
     });
 
+    lightSwitchApplication.ListContacts.prototype._$contentItems = {
+        Tabs: {
+            _$class: msls.ContentItem,
+            _$name: "Tabs",
+            _$parentName: "RootContentItem",
+            screen: lightSwitchApplication.ListContacts
+        },
+        ContactList: {
+            _$class: msls.ContentItem,
+            _$name: "ContactList",
+            _$parentName: "Tabs",
+            screen: lightSwitchApplication.ListContacts,
+            data: lightSwitchApplication.ListContacts,
+            value: lightSwitchApplication.ListContacts
+        },
+        Popups: {
+            _$class: msls.ContentItem,
+            _$name: "Popups",
+            _$parentName: "RootContentItem",
+            screen: lightSwitchApplication.ListContacts
+        }
+    };
+
+    msls._addEntryPoints(lightSwitchApplication.ListContacts, {
+        /// <field>
+        /// Called when a new ListContacts screen is created.
+        /// <br/>created(msls.application.ListContacts screen)
+        /// </field>
+        created: [lightSwitchApplication.ListContacts],
+        /// <field>
+        /// Called before changes on an active ListContacts screen are applied.
+        /// <br/>beforeApplyChanges(msls.application.ListContacts screen)
+        /// </field>
+        beforeApplyChanges: [lightSwitchApplication.ListContacts],
+        /// <field>
+        /// Called after the ContactList content item has been rendered.
+        /// <br/>postRender(HTMLElement element, msls.ContentItem contentItem)
+        /// </field>
+        ContactList_postRender: [$element, function () { return new lightSwitchApplication.ListContacts().findContentItem("ContactList"); }]
+    });
+
     lightSwitchApplication.Reports.prototype._$contentItems = {
         Tabs: {
             _$class: msls.ContentItem,
--- Cliente_CI.HTMLClient/GeneratedArtifacts/viewModel.js
+++ Cliente_CI.HTMLClient/GeneratedArtifacts/viewModel.js
@@ -27,6 +27,25 @@
         $Screen.call(this, dataWorkspace, "Home", parameters);
     }
 
+    function ListContacts(parameters, dataWorkspace) {
+        /// <summary>
+        /// Represents the ListContacts screen.
+        /// </summary>
+        /// <param name="parameters" type="Array">
+        /// An array of screen parameter values.
+        /// </param>
+        /// <param name="dataWorkspace" type="msls.application.DataWorkspace" optional="true">
+        /// An existing data workspace for this screen to use. By default, a new data workspace is created.
+        /// </param>
+        /// <field name="details" type="msls.application.ListContacts.Details">
+        /// Gets the details for this screen.
+        /// </field>
+        if (!dataWorkspace) {
+            dataWorkspace = new lightSwitchApplication.DataWorkspace();
+        }
+        $Screen.call(this, dataWorkspace, "ListContacts", parameters);
+    }
+
     function Reports(parameters, dataWorkspace) {
         /// <summary>
         /// Represents the Reports screen.
@@ -52,6 +71,10 @@
         ], [
         ]),
 
+        ListContacts: $defineScreen(ListContacts, [
+        ], [
+        ]),
+
         Reports: $defineScreen(Reports, [
         ], [
         ]),
@@ -66,6 +89,18 @@
             /// <returns type="WinJS.Promise" />
             var parameters = Array.prototype.slice.call(arguments, 0, 0);
             return lightSwitchApplication.showScreen("Home", parameters, options);
+        }),
+
+        showListContacts: $defineShowScreen(function showListContacts(options) {
+            /// <summary>
+            /// Asynchronously navigates forward to the ListContacts screen.
+            /// </summary>
+            /// <param name="options" optional="true">
+            /// An object that provides one or more of the following options:<br/>- beforeShown: a function that is called after boundary behavior has been applied but before the screen is shown.<br/>+ Signature: beforeShown(screen)<br/>- afterClosed: a function that is called after boundary behavior has been applied and the screen has been closed.<br/>+ Signature: afterClosed(screen, action : msls.NavigateBackAction)
+            /// </param>
+            /// <returns type="WinJS.Promise" />
+            var parameters = Array.prototype.slice.call(arguments, 0, 0);
+            return lightSwitchApplication.showScreen("ListContacts", parameters, options);
         }),
 
         showReports: $defineShowScreen(function showReports(options) {
